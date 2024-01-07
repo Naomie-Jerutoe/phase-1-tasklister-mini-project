1461589417ff65bba9c51b86a71f740d815cdd13
Naomi Lagat <naomiejerutoe@gmail.com>
2024-01-07 14:53:55 +0300

    feat:complete project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form');
+  const taskInput  = document.getElementById('new-task-description');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', addTask);
+
+  function addTask(event){
+    event. preventDefault();
+    const taskDescription = taskInput.value;
+
+    const newTasks = document.createElement('li');
+      newTasks.textContent = taskDescription;
+
+    const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'X';
+
+    deleteButton.addEventListener('click', ()=>{
+      newTasks.remove();
+    });
+
+    newTasks.appendChild(deleteButton);
+    taskList.appendChild(newTasks);
+    taskInput.value= "";
+  }
 });
